08a20eb2cef93730872196a9e57873830e8694aa
Aryan Kumar <kumar.aryan@gmail.com>
2022-09-24 16:55:58 -0700

    added ship class

--- runGame.py
+++ runGame.py
@@ -2,12 +2,14 @@ from cProfile import run
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
     settings = Settings()
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption("Starship")
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,7 @@ def run_game():
                 sys.exit()
 
         screen.fill(settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
     
 run_game()
